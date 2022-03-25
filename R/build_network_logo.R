aa3aeb1d2b54b7247f2a6a37ebc6809a088da7c6
sruizcarmona <sruizcarmona@gmail.com>
2022-03-25 13:49:55 +1100

    consolidated new network function to replace old one

--- R/build_network_logo.R
+++ R/build_network_logo.R
@@ -8,15 +8,21 @@
 #'
 #' @return Interactive force-directed layout network of your CV data
 #' @export
-build_network_logo <- function(position_data){
-
+build_network_logo_src <- function(position_data){
+  
   positions <- position_data %>%
     dplyr::mutate(
       id = dplyr::row_number(),
       title = stringr::str_remove_all(title, '(\\(.+?\\))|(\\[)|(\\])'),
       section = stringr::str_replace_all(section, "_", " ") %>% stringr::str_to_title()
-    )
-
+    ) %>% 
+    dplyr::rowwise() %>% 
+    dplyr::mutate(where = dplyr::case_when(
+      min(start_year, end_year) < 2019 ~ "Barcelona, ES",
+      TRUE ~ "Melbourne, AUS")) %>% 
+    dplyr::mutate(timeline = paste0(rev(strsplit(timeline, " - ")[[1]]), collapse=" - ")) %>% 
+    dplyr::ungroup()
+  
   combination_indices <- function(n){
     rep_counts <- (n:1) - 1
     dplyr::tibble(
@@ -48,22 +54,23 @@ build_network_logo <- function(position_data){
           target = ids_for_year$id[b]
         )
     })
-
-  network_data <- list(nodes = dplyr::select(positions, -in_resume,-timeline),
+  
+  network_data <- list(nodes = dplyr::select(positions, -in_resume),
                        edges = edges) %>%
     jsonlite::toJSON()
-
-  viz_script <- readr::read_file(system.file("js/cv_network.js", package = "datadrivencv"))
-
+  
+  viz_script <- readr::read_file(system.file("js/cv_network_src.js", package = "datadrivencv"))
+  
   glue::glue(
     "<script id = \"data_for_network\" type = \"application/json\">",
     "{network_data}",
     "</script>",
     "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3/5.16.0/d3.min.js\"></script>",
-    "<svg id = \"cv_network_viz\"></svg>",
+    "<svg angle=\"90\" rx=\"100\" ry=\"150\" viewBox=\"-10 0 215 270\" id = \"cv_network_viz\"></svg>",
     "<script>",
     "{viz_script}",
-    "</script>"
+    "</script>",
   )
-
 }
+
+# https://chartio.com/resources/tutorials/how-to-resize-an-svg-when-the-window-is-resized-in-d3-js/
